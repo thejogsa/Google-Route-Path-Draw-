488f21df768846734d01fccc025803704479890d
apple <apple@apples-Mac-mini-3.local>
2014-08-04 14:56:58 +0530

    Initial Commit

---
+++ Google Path/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Google Path
+//
+//  Created by apple on 04/08/14.
+//  Copyright (c) 2014 SAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
